6a66803a63a5745ce7a1c4761566607df4ae3d0a
Vijay Mahrra <vijay.mahrra@keycom.co.uk>
2015-10-06 14:27:29 +0100

    initial import

---
+++ CLI/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FFMVC\CLI;
+
+/**
+ * Index CLI Class.
+ *
+ * @author Vijay Mahrra <vijay@yoyo.org>
+ * @copyright (c) Copyright 2015 Vijay Mahrra
+ * @license GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ */
+class Index extends Base
+{
+    final public function index($f3, $params)
+    {
+        echo "CLI\n"."Hello World!\n";
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# FFMVC
+
+Files used by the https://github.com/vijinho/f3-boilerplate project.
+
+This small library of classes can be used to start your own project based on the code in
+f3-boilerplate.
+
+Vijay Mahrra
+http://about.me/vijay.mahrra
+----
