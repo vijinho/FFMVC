b03ad2e9a03a931681c8a1155728ed7fcd69e3fa
Vijay Mahrra <vijay.mahrra@gmail.com>
2016-07-18 23:33:46 +0100

    new mail helper. method to fetch a pre-configured php mailer instance

---
+++ Helpers/Mail.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace FFMVC\Helpers;
+
+/**
+ * Mail Helper Class.
+ *
+ * @author Vijay Mahrra <vijay@yoyo.org>
+ * @copyright (c) Copyright 2015 Vijay Mahrra
+ * @license GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ */
+class Mail extends \Prefab
+{
+    /**
+     * Return an instance of PHPMailer populated with application settings
+     *
+     * @param array $data
+     * @return \PHPMailer
+     */
+    final public static function getPhpMailer(array $data = [])
+    {
+        $f3 = \Base::instance();
+
+        $mail = new \PHPMailer();
+        $mail->isSMTP();
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->Username = $f3->get('email.username');
+        $mail->Password = $f3->get('email.password');
+        $mail->Port = $f3->get('email.port');
+        $mail->Host = $f3->get('email.host');
+        $mail->Timeout = $f3->get('email.timeout');
+        if ($f3->get('email.sendmail')) {
+            $mail->Sendmail = 'smtp://' .  $f3->get('email.host') . ':' . $f3->get('email.port');
+        }
+        $mail->FromName = $f3->get('email.from_name');
+        $mail->From = $f3->get('email.from');
+
+        // PHPMailer doesn't take 'To' so we have to do that with addAddress()
+        if (array_key_exists('To', $data)) {
+            $mail->addAddress($data['To']);
+            unset($data['To']);
+        }
+
+        // finally set other values like overrides
+        foreach ($data as $k => $v) {
+            $mail->$k = $v;
+        }
+
+        return $mail;
+    }
+}
