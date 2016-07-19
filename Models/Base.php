eb3ae5c8439964117ea6653cf3c5af2ae69817a4
Vijay Mahrra <vijay.mahrra@gmail.com>
2016-07-19 12:43:40 +0100

    have a separate Base and datatabase base model class

--- Models/Base.php
+++ Models/Base.php
@@ -16,22 +16,6 @@ abstract class Base extends \Prefab
 {
 
     /**
-     * @var object database class
-     */
-    protected $db;
-
-    /**
-     * @var table for the model
-     */
-    protected $table;
-
-    /**
-     * @var fat free database mapper
-     * @url http://fatfreeframework.com/databases#CRUD(ButWithaLotofStyle)
-     */
-    protected $mapper = null;
-
-    /**
      * @var object logging class
      */
     protected $logger;
@@ -45,37 +29,12 @@ abstract class Base extends \Prefab
         foreach ($params as $k => $v) {
             $this->$k = $v;
         }
-        if (empty($this->db)) {
-            $this->db = \Registry::get('db');
-
-            // guess the table name from the class name if not specified as a class member
-            $table = strtolower(empty($this->table) ? $f3->snakecase(substr(get_class($this),
-                            13)) : $this->table);
-            $this->table = $table;
-
-            // create a mapper in the f3 hive for the table
-            $mapper = new \DB\SQL\Mapper($this->db, $table);
-            $this->mapper = $mapper;
-            $f3->set($f3->camelcase($table) . 'Mapper', $mapper);
-        }
         if (empty($this->logger)) {
             $this->logger = &$f3->ref('logger');
         }
     }
 
 
-    /**
-     * Get row for field by given value
-     *
-     * @param string $field field to match
-     * @param string $value value to match
-     *
-     * @return object
-     */
-    final public function getBy($field, $value)
-    {
-        return $this->mapper->load(['`' . $field . '` = ?', $value]);
-    }
 }
 
 
---
+++ Models/BaseDb.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace FFMVC\Models;
+
+use \FFMVC\Helpers as Helpers;
+
+
+/**
+ * Base Database Model Class.
+ *
+ * @author Vijay Mahrra <vijay@yoyo.org>
+ * @copyright (c) Copyright 2015 Vijay Mahrra
+ * @license GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ */
+abstract class BaseDb extends Base
+{
+
+    /**
+     * @var object database class
+     */
+    protected $db;
+
+    /**
+     * @var table for the model
+     */
+    protected $table;
+
+    /**
+     * @var fat free database mapper
+     * @url http://fatfreeframework.com/databases#CRUD(ButWithaLotofStyle)
+     */
+    protected $mapper = null;
+
+    /**
+     * @var object logging class
+     */
+    protected $logger;
+
+    /**
+     * initialize.
+     */
+    public function __construct($params = [])
+    {
+        $f3 = \Base::instance();
+        foreach ($params as $k => $v) {
+            $this->$k = $v;
+        }
+        if (empty($this->db)) {
+            $this->db = \Registry::get('db');
+
+            // guess the table name from the class name if not specified as a class member
+            $table = strtolower(empty($this->table) ? $f3->snakecase(substr(get_class($this),
+                            13)) : $this->table);
+            $this->table = $table;
+
+            // create a mapper in the f3 hive for the table
+            $mapper = new \DB\SQL\Mapper($this->db, $table);
+            $this->mapper = $mapper;
+            $f3->set($f3->camelcase($table) . 'Mapper', $mapper);
+        }
+        if (empty($this->logger)) {
+            $this->logger = &$f3->ref('logger');
+        }
+    }
+
+
+    /**
+     * Get row for field by given value
+     *
+     * @param string $field field to match
+     * @param string $value value to match
+     *
+     * @return object $mapper object
+     */
+    public function getBy($field, $value)
+    {
+        return $this->mapper->load(['`' . $field . '` = ?', $value]);
+    }
+
+
+    /**
+     * Get a row by ID or UUID
+     *
+     * @param string $id
+     *
+     * @return array $data
+     *
+     * @throws BaseClientException
+     * @throws BaseServerException
+     */
+    public function getById($id)
+    {
+        $db = \Registry::get('db');
+
+        try {
+            $sql = 'SELECT * FROM ' . $this->table . ' WHERE id = :id OR uuid = :id';
+            $data = $db->exec($sql, [':id' => $id]);
+
+            return empty($data) ? false : $data[0];
+        } catch (\Exception $e) {
+            if ($e instanceof BaseClientException) {
+                throw new BaseDbServerException($e);
+            } else {
+                throw new BaseDbServerException($e);
+            }
+        }
+    }
+
+
+}
+
+
+class BaseDbClientException extends \Exception
+{
+
+}
+
+
+class BaseDbServerException extends \Exception
+{
+
+}
