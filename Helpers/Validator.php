d68fae8e6c0b47ce2c72a8a369583a0964ea44f1
Vijay Mahrra <vijay.mahrra@gmail.com>
2016-07-21 11:09:50 +0100

    add new validator filters to GUMP inheriting class

--- Helpers/Validator.php
+++ Helpers/Validator.php
@@ -2,6 +2,7 @@
 
 namespace FFMVC\Helpers;
 
+
 /**
  * Validation Helper Class
  *
@@ -9,11 +10,14 @@ namespace FFMVC\Helpers;
  * @author Vijay Mahrra <vijay@yoyo.org>
  * @copyright (c) Copyright 2015 Vijay Mahrra
  * @license GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ * @link https://github.com/Wixel/GUMP
  */
 class Validator extends \GUMP
 {
+
     /**
      *  A custom filter named "lower".
+     *
      *  The callback function receives two arguments:
      *  The value to filter, and any parameters used in the filter rule. It should returned the filtered value.
      *
@@ -26,4 +30,107 @@ class Validator extends \GUMP
     {
         return strtolower($value);
     }
+
+
+    /**
+     *  A custom filter named "upper".
+     *
+     *  The callback function receives two arguments:
+     *  The value to filter, and any parameters used in the filter rule. It should returned the filtered value.
+     *
+     * @param type $value
+     * @param type $param
+     *
+     * @return type
+     */
+    public function filter_upper($value, $param = null)
+    {
+        return strtoupper($value);
+    }
+
+    /**
+     * Strip whitespaces from the beginning of a string
+     *
+     * The callback function receives two arguments:
+     * The value to filter, and any parameters used in the filter rule. It should returned the filtered value.
+     *
+     * @param type $value
+     * @param type $param
+     *
+     * @return type
+     * @link https://fatfreeframework.com/utf-unicode-string-manager#ltrim
+     */
+    public function ltrim($value, $param = null)
+    {
+        return \UTF::instance()->ltrim($value);
+    }
+
+    /**
+     * Strip whitespaces from the end of a string
+     *
+     * The callback function receives two arguments:
+     * The value to filter, and any parameters used in the filter rule. It should returned the filtered value.
+     *
+     * @param type $value
+     * @param type $param
+     *
+     * @return type
+     * @link https://fatfreeframework.com/utf-unicode-string-manager#rtrim
+     */
+    public function rtrim($value, $param = null)
+    {
+        return \UTF::instance()->rtrim($value);
+    }
+
+    /**
+     * Strip whitespaces from the beginning and end of a string
+     *
+     * The callback function receives two arguments:
+     * The value to filter, and any parameters used in the filter rule. It should returned the filtered value.
+     *
+     * @param type $value
+     * @param type $param
+     *
+     * @return type
+     * @link https://fatfreeframework.com/utf-unicode-string-manager#trim
+     */
+    public function trim($value, $param = null)
+    {
+        return \UTF::instance()->trim($value);
+    }
+
+    /**
+     * Convert code points to Unicode symbols
+     *
+     * The callback function receives two arguments:
+     * The value to filter, and any parameters used in the filter rule. It should returned the filtered value.
+     *
+     * @param type $value
+     * @param type $param
+     *
+     * @return type
+     * @link https://fatfreeframework.com/utf-unicode-string-manager#translate
+     */
+    public function translate($value, $param = null)
+    {
+        return \UTF::instance()->translate($value);
+    }
+
+    /**
+     * Translate emoji tokens to Unicode font-supported symbols
+     *
+     * The callback function receives two arguments:
+     * The value to filter, and any parameters used in the filter rule. It should returned the filtered value.
+     *
+     * @param type $value
+     * @param type $param
+     *
+     * @return type
+     * @link https://fatfreeframework.com/utf-unicode-string-manager#emojify
+     */
+    public function emojify($value, $param = null)
+    {
+        return \UTF::instance()->emojify($value);
+    }
+
 }
