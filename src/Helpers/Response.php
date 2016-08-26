34bab5fdbee878c2b95945b289f95aaed4491964
Vijay Mahrra <vijay.mahrra@gmail.com>
2016-08-26 15:30:21 +0100

    tidy up method

--- src/Helpers/Response.php
+++ src/Helpers/Response.php
@@ -16,55 +16,55 @@ class Response extends \Prefab
      *
      * @param mixed $data   input variable, takes origin, age, methods
      * @param array $params parameters for the http headers: ttl, origin, methods (GET, POST, PUT, DELETE)
-     * @param bool  $output send the output headers and body? or return them?
      *
      * @return array<string,array|null|string>|null (array headers, string body)
      *
-     * @see http://www.w3.org/TR/2008/WD-access-control-20080912/
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+     * @see https://www.w3.org/TR/cors/
      */
-    public static function json(array $data, array $params = [], bool $output = true)
+    public static function json(array $data, array $params = [])
     {
         $f3 = \Base::instance();
 
+        $body    = json_encode($data, JSON_PRETTY_PRINT);
         $headers = array_key_exists('headers', $params) ? $params['headers'] : [];
-        $headers['Content-type'] = 'application/json; charset=utf-8';
 
-        $ttl = array_key_exists('ttl', $params) ? $params['ttl'] : 0; // cache for $ttl seconds
+        // set ttl
+        $ttl = (int) array_key_exists('ttl', $params) ? $params['ttl'] : 0; // cache for $ttl seconds
+
+        $headers = array_merge($headers, [
+            'Content-type'                     => 'application/json; charset=utf-8',
+            'Expires'                          => Time::http(time() + $ttl),
+            'Access-Control-Max-Age'           => $ttl,
+            'Access-Control-Expose-Headers'    => array_key_exists('acl_expose_headers', $params) ? $params['acl_expose_headers'] : null,
+            'Access-Control-Allow-Methods'     => array_key_exists('acl_http_methods', $params) ? $params['acl_http_methods'] : null,
+            'Access-Control-Allow-Origin'      => array_key_exists('acl_origin', $params) ? $params['acl_origin'] : '*',
+            'Access-Control-Allow-Credentials' => array_key_exists('acl_credentials', $params) && !empty($params['acl_credentials']) ?  'true' : 'false',
+            'ETag'                             => array_key_exists('etag', $params) ? $params['etag'] : md5($body),
+            'Content-Length'                   => \UTF::instance()->strlen($body),
+        ]);
+
+        // send the headers + data
+
         if (empty($ttl)) {
             $f3->expire(0);
-            //$headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0';
             $ttl = 0;
         }
 
-        $headers['Expires'] = Time::http(array_key_exists('expires', $params) ? $params['expires'] : time() + $ttl);
-        $headers['Access-Control-Max-Age'] = $ttl;
-        $headers['Access-Control-Allow-Methods'] = array_key_exists('http_methods', $params) ? $params['http_methods'] : ''; //'OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE';
-        $headers['Access-Control-Allow-Origin'] = array_key_exists('acl_origin', $params) ? $params['acl_origin'] : '*';
-        $headers['Access-Control-Allow-Credentials'] = array_key_exists('acl_credentials', $params) ? $params['credentials'] : 'false';
-
-        $body = json_encode($data, JSON_PRETTY_PRINT);
-        $headers['ETag'] = array_key_exists('etag', $params) ? $params['etag'] : md5($body);
-        $headers['Content-Length'] = \UTF::instance()->strlen($body);
-
-        // if returning output data or sending a HEAD request...
-        if (empty($output)) {
-            if ('HEAD' == $f3->get('VERB')) {
-                $body = null;
-            }
-            return ['headers' => $headers, 'body' => $body];
-        }
+        // default status is 200 - OK
+        $f3->status(array_key_exists('http_status', $params) ? $params['http_status'] : 200);
 
         // do not send session cookie
         if (!array_key_exists('cookie', $params)) {
             header_remove('Set-Cookie'); // prevent php session
         }
 
-        // default status is 200 - OK
-        $f3->status(array_key_exists('http_status', $params) ? $params['http_status'] : 200);
-
-        // send the headers + data
+        ksort($headers);
         foreach ($headers as $header => $value) {
-            header($header.': '.$value);
+            if (!isset($value)) {
+                continue;
+            }
+            header($header . ': ' . $value);
         }
 
         // HEAD request should be identical headers to GET request but no body
